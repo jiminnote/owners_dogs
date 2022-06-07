0283c3e6b0e11d37074203df1f924b32cd58021e
jeongjimin <wlals3968@naver.com>
2022-06-07 14:19:30 +0900

    ADD : models.py & view.py & urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from products.models import Owner, Dog
+
+class ProductsView(View):
+    def post(self, request):
+        data     = json.loads(request.body)
+        owner     = Owner.objects.create(name=data['owner'])
+        dog = Dog.objects.create(
+            name = data['dog'],
+            owner = owner
+        )
+        
+        return JsonResponse({'messasge':'created'}, status=201)
